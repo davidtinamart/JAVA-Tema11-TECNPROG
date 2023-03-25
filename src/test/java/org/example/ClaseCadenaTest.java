37881167c05cfb46153d16102172fa7c5e9d230d
davidtinamart <dmarttin@myuax.com>
2023-03-25 17:19:00 +0100

    Commit 7 - Acabado test

--- src/test/java/org/example/ClaseCadenaTest.java
+++ src/test/java/org/example/ClaseCadenaTest.java
@@ -1,7 +1,18 @@
 package org.example;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 class ClaseCadenaTest {
+
+
+    @org.junit.jupiter.api.Test
+
+    void DevuelveIniciales() {
+
+        String iniciales = ClaseCadena.DevuelveIniciales("David Tinaut");
+        assertEquals("D.T.", iniciales);
+
+        String iniciales2 = ClaseCadena.DevuelveIniciales(null);
+        assertEquals("", iniciales2);
+    }
 
 }

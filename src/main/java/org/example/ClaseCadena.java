bee2a3def8244f619799d70ea17165c2a6e877b4
davidtinamart <dmarttin@myuax.com>
2023-03-25 17:01:27 +0100

    Commit 3. Clase Cadena condicion si el nombre es nulo o vacio.

--- src/main/java/org/example/ClaseCadena.java
+++ src/main/java/org/example/ClaseCadena.java
@@ -1,4 +1,10 @@
 package org.example;
 
 public class ClaseCadena {
-}
+
+    public static String DevuelveIniciales(String nombreCompleto) { //devuelve un String
+        String iniciales = ""; //inicializamos la variable iniciales
+        if (nombreCompleto == null || nombreCompleto.isEmpty()) { //si el nombre es nulo o vacío
+            return iniciales; //devuelve una cadena vacía
+        }
+    }
